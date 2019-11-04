bdc6e37e0e86d32bf7ff7213515999d6d1a09395
Wesley Dashner <wdashner11@gmail.com>
2019-11-04 11:31:22 -0700

    initial commit

---
+++ Snake Game/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Snake Game
+//
+//  Created by Wesley Dashner on 11/4/19.
+//  Copyright © 2019 Wesley Dashner. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
